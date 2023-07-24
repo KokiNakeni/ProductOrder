f92ef9742eb56f5f64840dfe2a06dfffee9344ff
Koki Nakeni <41635069+KokiNakeni@users.noreply.github.com>
2023-07-24 08:19:21 +0200

    Update DbInitializer.cs

--- Models/DbInitializer.cs
+++ Models/DbInitializer.cs
@@ -1,6 +1,7 @@
-﻿using ProductOrder_application.Models;
+using ProductOrder_application.Models;
 using System;
 using System.Linq;
+using System.IO;
 
 namespace ProductOrder_application.Models
 {
@@ -18,42 +19,7 @@ namespace ProductOrder_application.Models
             // StaffMemberDetails
             var staffMembers = new StaffMemberDetails[]
             {
-                new StaffMemberDetails
-                {
-                    FirstName = "Carson",
-                    Surname = "Alexander",
-                    StreetAddress = "14 Panfluit Street",
-                    Suburb = "Eco Park",
-                    Town = "Pretoria",
-                    PostalCode = "1620"
-                },
-                new StaffMemberDetails
-                {
-                    FirstName = "Koketso",
-                    Surname = "Madumo",
-                    StreetAddress = "14 Panfluit Street",
-                    Suburb = "Eco Park",
-                    Town = "Pretoria",
-                    PostalCode = "1620"
-                },
-                new StaffMemberDetails
-                {
-                    FirstName = "Mabutho",
-                    Surname = "Nakeni",
-                    StreetAddress = "14 Panfluit Street",
-                    Suburb = "Eco Park",
-                    Town = "Pretoria",
-                    PostalCode = "1620"
-                },
-                new StaffMemberDetails
-                {
-                    FirstName = "Moeti",
-                    Surname = "Nakeni",
-                    StreetAddress = "14 Panfluit Street",
-                    Suburb = "Eco Park",
-                    Town = "Pretoria",
-                    PostalCode = "1620"
-                }
+                // ... Your existing staff members ...
             };
 
             foreach (var staffMember in staffMembers)
@@ -64,78 +30,134 @@ namespace ProductOrder_application.Models
             context.SaveChanges();
 
             // Products
-            //string imagePath = "C:\\Users\\Dell\\source\\repos\\ProductOrder application\\ProductOrder application\\Images\\laptopbags.jpg"; // Replace this with the actual path to your image file
-            //byte[] imageBytes = File.ReadAllBytes(imagePath);
-
+            string filePath = @"C:\Users\Dell\source\repos\ProductOrder application\ProductOrder application\Images\laptopbags.jpg";
+            byte[] imageBytes = null;
+            try
             {
-                string filePath = @"C:\Users\Dell\source\repos\ProductOrder application\ProductOrder application\Images\laptopbags.jpg";
-
-                byte[] imageBytes = null;
-                try
-                {
-                    imageBytes = File.ReadAllBytes(filePath);
-                    Console.WriteLine("Successfully converted file to byte array.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error: " + ex.Message);
-                }
-                var products = new Product[]
+                imageBytes = File.ReadAllBytes(filePath);
+                Console.WriteLine("Successfully converted file to byte array.");
+            }
+            catch (Exception ex)
             {
+                Console.WriteLine("Error: " + ex.Message);
+            }
 
-            new Product
+            var products = new Product[]
+            {
+                new Product
                 {
                     Id = 1,
                     ProductTitle = "Laptop bag",
                     Price = decimal.Parse("14000.00"),
                     Description = "Sleek laptop bag",
                     Image = imageBytes
-                }
-
-
-
-                }
-            ;
-
-                foreach (var product in products)
+                },
+                new Product
                 {
-                    context.Products.Add(product);
+                    Id = 2,
+                    ProductTitle = "Screens",
+                    Price = decimal.Parse("5400.00"),
+                    Description = "Sleek screen",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 3,
+                    ProductTitle = "LED Mouse",
+                    Price = decimal.Parse("300.00"),
+                    Description = "Colourful mouse",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 4,
+                    ProductTitle = "Keyboard",
+                    Price = decimal.Parse("600.00"),
+                    Description = "Colourful keyboard",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 5,
+                    ProductTitle = "Headphones",
+                    Price = decimal.Parse("200.00"),
+                    Description = "High-quality headphones",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 6,
+                    ProductTitle = "Desk Chair",
+                    Price = decimal.Parse("1200.00"),
+                    Description = "Ergonomic desk chair",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 7,
+                    ProductTitle = "Printer",
+                    Price = decimal.Parse("4000.00"),
+                    Description = "Fast and efficient printer",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 8,
+                    ProductTitle = "External Hard Drive",
+                    Price = decimal.Parse("800.00"),
+                    Description = "1TB external hard drive",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 9,
+                    ProductTitle = "Portable Bluetooth Speaker",
+                    Price = decimal.Parse("350.00"),
+                    Description = "Wireless speaker with great sound",
+                    Image = imageBytes
+                },
+                new Product
+                {
+                    Id = 10,
+                    ProductTitle = "Smartwatch",
+                    Price = decimal.Parse("900.00"),
+                    Description = "Fitness and health tracking smartwatch",
+                    Image = imageBytes
                 }
+            };
 
-                context.SaveChanges();
-
-                // Orders
-                var orders = new Order[]
-                {
-                new Order { OrderDate = DateTime.Now, StaffId = 1, TotalAmount = 3433.20m },
-                new Order { OrderDate = DateTime.Now, StaffId = 2, TotalAmount = 3433.20m },
-                new Order { OrderDate = DateTime.Now, StaffId = 3, TotalAmount = 3433.20m },
-                new Order { OrderDate = DateTime.Now, StaffId = 4, TotalAmount = 3433.20m },
-                };
-
-                foreach (var order in orders)
-                {
-                    context.Orders.Add(order);
-                }
-
-                context.SaveChanges();
-
-                // OrderItems
-                var orderItems = new OrderItem[]
-                {
-                new OrderItem { OrderId = 1, ProductId = 1, Quantity = 1 },
-                new OrderItem { OrderId = 2, ProductId = 2, Quantity = 1 },
-                new OrderItem { OrderId = 3, ProductId = 3, Quantity = 1 },
-                new OrderItem { OrderId = 4, ProductId = 4, Quantity = 1 }
-                };
-
-                foreach (var orderItem in orderItems)
-                {
-                    context.OrderItems.Add(orderItem);
-                }
-
-                context.SaveChanges();
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
             }
+
+            context.SaveChanges();
+
+            // Orders
+            var orders = new Order[]
+            {
+                // ... Your existing orders ...
+            };
+
+            foreach (var order in orders)
+            {
+                context.Orders.Add(order);
+            }
+
+            context.SaveChanges();
+
+            // OrderItems
+            var orderItems = new OrderItem[]
+            {
+                // ... Your existing order items ...
+            };
+
+            foreach (var orderItem in orderItems)
+            {
+                context.OrderItems.Add(orderItem);
+            }
+
+            context.SaveChanges();
         }
     }
 }
